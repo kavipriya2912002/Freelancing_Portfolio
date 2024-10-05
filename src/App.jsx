359fcf8f7b59086bd7bd2939d11a93b4291ba3a5
unknown <kavipriya2912002@gmail.com>
2024-10-05 04:53:23 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter , Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
-import Contact from './pages/contact'
+import Contact from './pages/Contact';
 function App() {
   return (
     <BrowserRouter>
